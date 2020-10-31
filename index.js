8ce8c5f768ab926b448feb79eaa58b34d8f5243b
jinho6225 <jinho6225@hotmail.com>
2020-10-31 02:01:45 -0700

    너무 많아

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-    // Add your code below this line
-    const scale = d3.scaleLinear();
+const positionData = [[1, 7, -4],[6, 3, 8],[2, 9, 3]]
+// Add your code below this line
 
-scale.domain([250, 500])
-scale.range([10, 150])
+const output = d3.max(positionData, (d) => d[2]);
+; // Change this line
 
-    // Add your code above this line
-    const output = scale(50);
-    d3.select("body")
-      .append("h2")
-      .text(output);
+// Add your code above this line
+
+d3.select("body")
+  .append("h2")
+  .text(output)
