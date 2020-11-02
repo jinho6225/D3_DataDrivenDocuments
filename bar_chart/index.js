47900e82c9047e0f2357cdfe3c33d070a939f1b6
jinho6225 <jinho6225@hotmail.com>
2020-11-01 16:04:45 -0800

    duplication removed using transform & translate

--- bar_chart/index.js
+++ bar_chart/index.js
@@ -1,22 +1,3 @@
-// d3.select("h3").style("color", "darkblue").style("font-size", "24px");
-// // d3.select("h3");
-
-// var fruits = ['apple', 'mango', 'banana', 'orange'];    
-// d3.select('ul')        
-// .selectAll('li')        
-// .data(fruits)        
-// .enter()        
-// .append('li')        
-// .text((d) => d);
-
-// var svg = d3.select('svg');
-// svg.append('rect')   
-// .attr('x', 200)   
-// .attr('y', 10)
-// .attr('width', 100)   
-// .attr('height', 100)   
-// .attr('fill', 'green');
-
 var data = [80, 120, 60, 150, 200];
 
 var barHeight = 20;
