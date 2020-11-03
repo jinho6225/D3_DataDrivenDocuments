e7f23d9117b565163c7c41e9a97dada65232130a
jinho6225 <jinho6225@hotmail.com>
2020-11-03 10:48:33 -0800

    drawing bar chart, wrote text on the bar chart

--- bar_chart/index.js
+++ bar_chart/index.js
@@ -1,79 +1,27 @@
 const URL = `https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json`
 
+var svg = d3.select('body').append('svg').attr('class', 'bar-chart')
 
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+var svgWidth = 500
+var svgHeight = 300
+var barPadding = 5;
+var barWidth = (svgWidth / dataset.length);
 
-const w = 1900;
-const h = 800;
-const padding = 50;
+svg.attr('width', svgWidth).attr('height', svgHeight);
 
-const svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h)
-            .attr('class', "bar-chart")
-
-
-fetch(URL).then(res => res.json())
-.then(data => {
-
-    let GDP = data.data.map(function (item) {
-        return item[1];
-    });
-    let gdpMax = d3.max(GDP);
-
-    const yScale = d3.scaleLinear()
-    .domain([0, gdpMax])
-    .range([0, h]);
-
-    svg.selectAll("rect")
-        .data(data.data)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => i * 5 + padding)
-        .attr("y", (d, i) => h - yScale(d[1]))
-        .attr("width", 3)
-        .attr("height", (d, i) => yScale(d[1]))
-        .attr('fill', 'navy')
-        .attr('class', 'bar')
-        .append("title")
-        .text((d) => d)
-
-    let yAxisScale = d3.scaleLinear().domain([0, gdpMax]).range([h, 0]);
-    let yAxis = d3.axisLeft(yAxisScale);
-    svg.append("g")
-        // .attr("transform", `translate(${padding}, ${h-padding})`)
-        .call(yAxis)
-        .attr('transform', `translate(${padding}, 0)`);  
-            
-    let years = data.data.map(function (item) {
-        let quarter;
-        let temp = item[0].substring(5, 7);
-
-        if (temp === '01') {
-        quarter = 'Q1';
-        } else if (temp === '04') {
-        quarter = 'Q2';
-        } else if (temp === '07') {
-        quarter = 'Q3';
-        } else if (temp === '10') {
-        quarter = 'Q4';
-        }
-
-        return item[0].substring(0, 4) + ' ' + quarter;
-    });
-
-    let yearsDate = data.data.map(function (item) {
-        return new Date(item[0]);
-    });
-
-    let xMax = new Date(d3.max(yearsDate));
-    xMax.setMonth(xMax.getMonth() + 3);
-    let xScale = d3
-        .scaleTime()
-        .domain([d3.min(yearsDate), xMax])
-        .range([0, w]);
-
-    let xAxis = d3.axisBottom().scale(xScale);
-            
+var barChart = svg.selectAll("rect").data(dataset).enter().append('rect')
+.attr('y', d => svgHeight - d)
+.attr('height', d => d)
+.attr('width', barWidth - barPadding)
+.attr('transform', (d, i) => {
+    var translate = [barWidth * i, 0]; 
+    return "translate("+ translate +")";
 })
 
+var text = svg.selectAll('text')
+.data(dataset).enter().append("text")
+.text(d => d)
+.attr('y', (d, i) => svgHeight - d - 2)
+.attr('x', (d, i) => barWidth * i)
+.attr('fill', 'navy')
