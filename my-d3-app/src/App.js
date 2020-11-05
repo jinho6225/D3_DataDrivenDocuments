a554e4d5a2c1a831deea62acd6584e63a4ed0e66
jinho6225 <jinho6225@hotmail.com>
2020-11-05 12:05:40 -0800

    line, curveCardinal, path, attr("d",~~)

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -1,36 +1,37 @@
-import React, { useRef, useEffect, useState } from 'react';
-import './App.css';
-import { select } from 'd3';
-
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { select, line, curveCardinal } from "d3";
 
 function App() {
-    const svgRef = useRef();
-    const [data, setData] = useState([12,23,34,37,56])
-    useEffect(() => {
-        console.log(svgRef)
-        const svg = select(svgRef.current);
-        svg.selectAll('circle')
-        .data(data)
-        .join('circle')
-        // .join(
-        //     enter => enter.append('circle').attr('class', 'new'),
-        //     update => update.attr('class', 'updated'),
-        //     exit => exit.remove()
-        // )
-        .attr('r', val => val)
-        .attr('cx', val => val * 2)
-        .attr('cy', val => val * 2)
-        .attr('stroke', 'red')
-        ;
-    }, [data])
+  const svgRef = useRef();
+  const [data, setData] = useState([12, 23, 37, 56, 33, 110, 44]);
 
-    return (
+  useEffect(() => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    const myLine = line()
+      .x((val, i) => i * 50)
+      .y((val) => 150 - val)
+      .curve(curveCardinal);
+
+    svg
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", (val) => myLine(val))
+      .attr("fill", "none")
+      .attr("stroke", "red");
+  }, [data]);
+
+  return (
     <>
-    <svg ref={svgRef}></svg>
-    <br />
-    <button onClick={() => setData(data.map(val => val + 5))}>Add</button>
-    <button onClick={() => setData(data.filter(val => val < 35))}>Filter</button>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map((val) => val + 5))}>Add</button>
+      <button onClick={() => setData(data.filter((val) => val < 35))}>
+        Filter
+      </button>
     </>
-    )
+  );
 }
 export default App;
