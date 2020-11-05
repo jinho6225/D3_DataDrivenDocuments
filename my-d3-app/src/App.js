b371650d994e441d881d6fca102fb210d638c7b5
jinho6225 <jinho6225@hotmail.com>
2020-11-05 12:28:17 -0800

    axisBottom, scaleLinear, axisRight, xAxis, yAxis, xScale, yScale

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -1,23 +1,46 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  scaleLinear,
+  axisRight,
+} from "d3";
 
 function App() {
   const svgRef = useRef();
   const [data, setData] = useState([12, 23, 37, 56, 33, 110, 44]);
 
   useEffect(() => {
-    console.log(svgRef);
     const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
+    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
+    svg.select(".x-axis")
+    .style("transform", "translateY(150px)")
+    .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select('.y-axis')
+    .style("transform", "translateX(300px)")
+    .call(yAxis);
+
     const myLine = line()
-      .x((val, i) => i * 50)
-      .y((val) => 150 - val)
+      .x((val, i) => xScale(i))
+      .y((val) => yScale(val))
       .curve(curveCardinal);
 
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
+      .attr("class", "line")
       .attr("d", (val) => myLine(val))
       .attr("fill", "none")
       .attr("stroke", "red");
@@ -25,7 +48,12 @@ function App() {
 
   return (
     <>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <br />
+      <br />
       <br />
       <button onClick={() => setData(data.map((val) => val + 5))}>Add</button>
       <button onClick={() => setData(data.filter((val) => val < 35))}>
