f111b9cf4658a677ce214e9ff970e4e83dc2f933
jinho6225 <jinho6225@hotmail.com>
2020-11-05 11:39:04 -0800

    initial point

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -3,14 +3,6 @@ import CSVReader from 'react-csv-reader'
 
 
 function App() {
-    const [data, setData] = useState([])
-
-    console.log(data)
-    return <div>hello
-        <CSVReader onFileLoaded={(data, fileInfo) => {
-            setData(data.slice(1))
-            console.dir(data, fileInfo)
-        }} />
-    </div>
+    return <svg></svg>
 }
 export default App;
