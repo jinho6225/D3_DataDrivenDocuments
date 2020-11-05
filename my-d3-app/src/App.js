ad086d06d76d7010d6964100321bfd98529775eb
jinho6225 <jinho6225@hotmail.com>
2020-11-05 15:30:16 -0800

    pie chart

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -2,45 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import PieChart from "./PieChart";
 
+const data = [{ label: 'Apples', value: 10 }, { label: 'Oranges', value: 20 }];
+
 function App() {
-    const [data, setData] = useState([
-        {
-            name: "apples",
-            value: 100
-        },
-        {
-            name: "bananas",
-            value: 300
-        },
-        {
-            name: "cherries",
-            value: 250
-        }
-    ]);
 
   return (
     <>
         <h1>Pie Chart</h1>
-        <PieChart data={data} />
-        <button onClick={() => {
-                setData([
-                    {
-                        name: "apples",
-                        value: Math.round(Math.random() * 100)
-                    },
-                    {
-                        name: "bananas",
-                        value: Math.round(Math.random() * 200)
-                    },
-                    {
-                        name: "cherries",
-                        value: Math.round(Math.random() * 300)
-                    }
-                ]);
-            }}
-        >
-            Randomize
-        </button>    
+        <PieChart data={data}/>
     </>
   );
 }
--- my-d3-app/src/PieChart.js
+++ my-d3-app/src/PieChart.js
@@ -1,51 +1,54 @@
-import { arc, pie, select, scaleOrdinal, schemeGreens } from "d3";
-import React, { useEffect, useRef } from "react";
-import useResizeObserver from "./useResizeObserver";
+import React, { useEffect } from 'react';
+import * as d3 from 'd3'
 
-function PieChart({ data }) {
-  const svgRef = useRef();
-  const wrapperRef = useRef();
-  const dimensions = useResizeObserver(wrapperRef);
+function PieChart(props) {
+    const {data} = props
+    const outerRadius = 150
+    const innerRadius = 0
+    const margin = {
+        top: 50, right: 50, bottom: 50, left: 50,
+    }
 
-  // will be called initially and on every data change
-  useEffect(() => {
-    const svg = select(svgRef.current);
-    if (!dimensions) return;
+    const width = 2 * outerRadius + margin.left + margin.right;
+    const height = 2 * outerRadius + margin.top + margin.bottom;
 
-    // arc takes instructions (objects with special properties, like startAngle, endAngle, etc.)
-    // and transforms them into "d" attributes for path elements
-    const arcGenerator = arc()
-    .innerRadius(0)
-    .outerRadius(150);
+    useEffect(() => {
+        drawChart();
+    }, [data])
 
-    // pie will transform data to instructions for arcGenerator
-    const pieGenerator = pie()
-    .value(d => d.value)
-    // .sort(null); // makes sure data doesn't get sorted
+    function drawChart() {
+        const colorScale = d3.scaleSequential().interpolator(d3.interpolateCool).domain([0, data.length])
+        const arcGenerator = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius)
+        const pieGenerator = d3.pie().padAngle(0).value(d => d.value)
+        
+        const svg = d3.select('#pie-container')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${width / 2}, ${height / 2})`)
 
-    // now transform data to instructions for arc() 
-    const instructions = pieGenerator(data)
+        const arc = svg.selectAll().data(pieGenerator(data)).enter();
 
-    const colorScale = scaleOrdinal(schemeGreens[3]);
+        arc
+        .append('path')
+        .attr('d', arcGenerator)
+        .style('fill', (_, i) => colorScale(i))
+        .style('stroke', '#ffffff')
+        .style('stroke-width', 0);
 
+        arc
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'middle')
+        .text((d) => d.data.label)
+        .style('fill', '#ffffff')
+        .attr('transform', (d) => {
+          const [x, y] = arcGenerator.centroid(d);
+          return `translate(${x}, ${y})`;
+        });
+    }
 
-    svg
-        .selectAll(".slice")
-        .data(instructions)
-        .join("path")
-        .attr("class", 'slice')
-        .attr("stroke", 'black')
-        .attr("fill", (d, i) => colorScale(i))
-        .style('transform', `translate(${dimensions.width / 2}px, ${dimensions.height / 2}px)`)
-        .attr('d', instruction => arcGenerator(instruction))
-        .transition()
-  }, [data, dimensions]);
-
-  return (
-    <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
-      <svg ref={svgRef}></svg>
-    </div>
-  );
+    return <div id="pie-container" />;
 }
-
 export default PieChart;
