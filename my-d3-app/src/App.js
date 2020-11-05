3fc783d5ffedba8f4f6ef75ad90a66f36574ae27
jinho6225 <jinho6225@hotmail.com>
2020-11-05 12:51:02 -0800

    scaleBand, colorScale, transition, animated bar chart

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -1,49 +1,50 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import {
-  select,
-  line,
-  curveCardinal,
-  axisBottom,
-  scaleLinear,
-  axisRight,
-} from "d3";
+import { select, axisBottom, scaleLinear, axisRight, scaleBand } from "d3";
 
 function App() {
   const svgRef = useRef();
-  const [data, setData] = useState([12, 23, 37, 56, 33, 110, 44]);
+  const [data, setData] = useState([12, 23, 37, 90, 33, 110, 44]);
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    const xScale = scaleLinear()
-      .domain([0, data.length - 1])
-      .range([0, 300]);
+    const xScale = scaleBand()
+      //   .domain([0,1,2,3,4,5,6])
+      .domain(data.map((val, index) => index))
+      .range([0, 300])
+      .padding(0.5);
 
-    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+    const yScale = scaleLinear()
+    .domain([0, 150])
+    .range([150, 0]);
 
-    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1);
+    const colorScale = scaleLinear()
+    .domain([75, 100, 150])
+    .range(["green", "orange", "red"]);
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
+
     svg.select(".x-axis")
     .style("transform", "translateY(150px)")
     .call(xAxis);
 
     const yAxis = axisRight(yScale);
-    svg.select('.y-axis')
+    svg.select(".y-axis")
     .style("transform", "translateX(300px)")
     .call(yAxis);
 
-    const myLine = line()
-      .x((val, i) => xScale(i))
-      .y((val) => yScale(val))
-      .curve(curveCardinal);
+    svg.selectAll(".bar")
+    .data(data)
+    .join("rect")
+    .attr("class", "bar")
+    .style("transform", 'scale(1, -1)')
+    .attr('x', (val, i) => xScale(i))
+    .attr('y', -150)
+    .attr('width', xScale.bandwidth())
+    .transition()
+    .attr('fill', colorScale)
+    .attr('height', val => 150 - yScale(val))
 
-    svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class", "line")
-      .attr("d", (val) => myLine(val))
-      .attr("fill", "none")
-      .attr("stroke", "red");
   }, [data]);
 
   return (
@@ -52,9 +53,6 @@ function App() {
         <g className="x-axis" />
         <g className="y-axis" />
       </svg>
-      <br />
-      <br />
-      <br />
       <button onClick={() => setData(data.map((val) => val + 5))}>Add</button>
       <button onClick={() => setData(data.filter((val) => val < 35))}>
         Filter
