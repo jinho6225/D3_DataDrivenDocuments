5a40fd8993e3d7ed60aef5a4525a8b5cbadfaeb7
jinho6225 <jinho6225@hotmail.com>
2020-11-13 11:46:29 -0800

    tree cahrt, hierarchy, tree, treeLayout

--- my-d3-app/src/App.js
+++ my-d3-app/src/App.js
@@ -1,74 +1,41 @@
 import React, { useState } from "react";
-import RacingBarChart from "./RacingBarChart";
-import useInterval from "./useInterval";
+import TreeChart from "./TreeChart";
 import "./App.css";
 
-const getRandomIndex = array => {
-  return Math.floor(array.length * Math.random());
+const initialData = {
+  name: "😐",
+  children: [
+    {
+      name: "🙂",
+      children: [
+        {
+          name: "😀"
+        },
+        {
+          name: "😁"
+        },
+        {
+          name: "🤣"
+        }
+      ]
+    },
+    {
+      name: "😔"
+    }
+  ]
 };
 
 function App() {
-  const [iteration, setIteration] = useState(0);
-  const [start, setStart] = useState(false);
-  const [data, setData] = useState([
-    {
-      name: "a",
-      value: 10,
-      color: "#f4efd3"
-    },
-    {
-      name: "b",
-      value: 15,
-      color: "#cccccc"
-    },
-    {
-      name: "c",
-      value: 20,
-      color: "#c2b0c9"
-    },
-    {
-      name: "d",
-      value: 25,
-      color: "#9656a1"
-    },
-    {
-      name: "e",
-      value: 30,
-      color: "#fa697c"
-    },
-    {
-      name: "f",
-      value: 35,
-      color: "#fcc169"
-    }
-  ]);
-
-  useInterval(() => {
-    if (start) {
-      const randomIndex = getRandomIndex(data);
-      setData(
-        data.map((entry, index) =>
-          index === randomIndex
-            ? {
-                ...entry,
-                value: entry.value + 10
-              }
-            : entry
-        )
-      );
-      setIteration(iteration + 1);
-    }
-  }, 500);
+  const [data, setData] = useState(initialData);
 
   return (
-    <>
-      <h1>Racing Bar Chart</h1>
-      <RacingBarChart data={data} />
-      <button onClick={() => setStart(!start)}>
-        {start ? "Stop the race" : "Start the race!"}
+    <React.Fragment>
+      <h1>Animated Tree Chart</h1>
+      <TreeChart data={data} />
+      <button onClick={() => setData(initialData.children[0])}>
+        Update data
       </button>
-      <p>Iteration: {iteration}</p>
-    </>
+    </React.Fragment>
   );
 }
 
